f864c55a00bcb8dc1ba6f3c7e90fbda4e0aea58f
angelabair <angela.bair52@gmail.com>
2013-11-17 00:07:22 -0500

    Added script that creates popup boxes on scroll-over

---
+++ browser-extension/js/popup.js
@@ -0,0 +1,34 @@
+$(document).ready(function()
+{
+		var x = jQuery('<div/>', {
+		id: 'foo',
+		rel: 'external',
+		text: 'I did the thing.'
+		});
+		
+		x.css("font-family", "Verdana");
+		x.css("padding", "10px");
+		x.css("background","#EEEFEB");
+		x.css("margin","0px");
+		x.css("color","black");
+		x.css("border","2px");
+		x.css("border-style","solid");
+		x.css("border-color","#4D4F53");
+		x.css("position","absolute");
+		x.css("border-radius", "3px");
+		x.css("-webkit-box-shadow", "0px 0px 5px 1px rgba(164, 164, 164, 1)");
+		x.css("box-shadow", "0px 0px 5px 1px rgba(164, 164, 164, 1)");
+		
+	$('a.comments').hover(function(){
+		x.appendTo(this);
+		
+		$('a.comments').mousemove(function(e){
+			x.css({left:e.pageX, top:e.pageY});
+		});
+		
+	});
+	
+	$('a.comments').mouseleave(function(){
+			x.remove();
+		});
+});
